22cb0ff0ee01af857447e36762de138c967bd561
Abel Cotoñeto <abelcotoneto@Abels-MacBook-Pro.local>
2024-06-14 17:26:55 -0600

    fix: Fixed issue with 0 bytes download for backend

---
+++ professional-headshot-back/functions/download_images.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+import boto3
+import botocore
+from botocore import UNSIGNED
+from botocore.config import Config
+
+
+@dataclass
+class DownloadImages():
+    """
+    Class for downloading the images from a given S3 bucket
+    """
+
+    BUCKET_NAME: str = 'myBucket' 
+    PATH: str = 'images/' 
+
+    def process(self):
+        s3 = boto3.resource('s3', config=Config(signature_version=UNSIGNED))
+        try:
+            my_bucket = s3.Bucket(self.BUCKET_NAME)
+            for item in my_bucket.objects.filter(Prefix=self.PATH):
+                split_object = item.key.split('/')
+                if split_object[-1] != '':
+                    file = item.key.split(self.PATH)[1]
+                    print(file)
+                    my_bucket.download_file(self.PATH + file, file)
+        except botocore.exceptions.ClientError as e: 
+            if e.response['Error']['Code'] == "404":
+                print("The object does not exist.")
+            else:
+                raise
+        return self
+
+    def get(self) -> None:
+        return None
--- professional-headshot-back/main.py
+++ professional-headshot-back/main.py
@@ -1,70 +1,10 @@
 from fastapi import FastAPI, Form
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
-from functions.get_images import GetImages
+from functions.run_training import RunTraining
 import os
-import subprocess
-import boto3
-import botocore
-from botocore import UNSIGNED
-from botocore.config import Config
 
 app = FastAPI()
-
-def test(
-        job_name: str =  "MyTuneJob",
-        classname: str = "man",
-        folder_path: str = "Headshots"):
-    """
-    Define the initial values
-    """
-    directory = os.getcwd()
-    folder_path = ""
-    command_list = ["python", "functions/astria.py",
-                    "tune", job_name, classname]
-
-    """
-    Get the images from the folder
-    """
-
-    get_paths()
-
-    images = (
-        GetImages(
-            folder_name=folder_path,
-            directory=directory
-            )
-        .process()
-        .get()
-    )
-
-    command_list.extend(images)
-
-    """
-    Call astria.py file that is in the functions folder
-    """
-    subprocess.run(command_list)
-
-def get_paths():
-
-    BUCKET_NAME = 'backend-professional-headshot-test-avahi' 
-    PATH = 'images/' 
-
-    s3 = boto3.resource('s3', config=Config(signature_version=UNSIGNED))
-
-    try:
-        my_bucket = s3.Bucket(BUCKET_NAME)
-        for item in my_bucket.objects.filter(Prefix=PATH):
-            split_object = item.key.split('/')
-            if split_object[-1] != '':
-                file = item.key.split(PATH)[1]
-                print(file)
-                s3.Bucket(BUCKET_NAME).download_file(PATH, file)
-    except botocore.exceptions.ClientError as e: 
-        if e.response['Error']['Code'] == "404":
-            print("The object does not exist.")
-        else:
-            raise
 
 # Allow CORS for all origins (for simplicity)
 app.add_middleware(
@@ -88,14 +28,17 @@ async def submit_strings(
     classname: str = Form(...),
     folder_path: str = Form(...)
 ):
-    print(f"This is the API KEY: {api_key}")
     os.environ['ASTRIA_API_TOKEN'] = api_key
-    print(f"This is the Job Name: {job_name}")
-    print(f"This is the Classname: {classname}")
-    print(f"This is the Folder Path: {folder_path}")
-    test(job_name=job_name,
-         classname=classname,
-         folder_path=folder_path)
+
+    (
+        RunTraining(
+            job_name=job_name,
+            classname=classname
+        )
+        .process()
+        .get()
+    )
+
     return {"message": "Success!"}
 
 

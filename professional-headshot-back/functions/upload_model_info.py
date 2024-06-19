24d6cd061624d5eb57f4c775b5dadaa2c4530e5b
Abel Cotoñeto <abelcotoneto@Abels-MacBook-Pro.local>
2024-06-19 12:52:01 -0600

    fix: Deleted testing samples from upload_model_info

--- professional-headshot-back/functions/upload_model_info.py
+++ professional-headshot-back/functions/upload_model_info.py
@@ -14,23 +14,9 @@ class UploadModelInfo:
         s3 = boto3.resource('s3', config=Config(signature_version=UNSIGNED))
         key = f'{self.object_prefix}{self.response_json["title"]}/{str(self.response_json["id"])}.txt'
         # s3.Bucket(self.bucket).upload_file(self.response_json["id"], object_name)
-        s3.Object(bucket, key).put(Body='')
+        s3.Object(self.bucket, key).put(Body='')
         # s3.Bucket(self.bucket).upload_file(self.response_json["id"], object_name)
         return self
 
     def get(self) -> None:
         return None
-
-response_json = {'id': 5678, 'title': 'Modelo-Abel-2'}
-bucket = 'backend-professional-headshot-test-avahi'
-object_prefix = 'AbelModel' + '/model-info/'
-
-(
-    UploadModelInfo(
-        response_json=response_json,
-        bucket=bucket,
-        object_prefix=object_prefix
-    )
-    .process()
-    .get()
-)

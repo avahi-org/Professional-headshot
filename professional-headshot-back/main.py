d453942fe79e09e77aed81d2b9c467ec309d3ae7
Abel Cotoñeto <abelcotoneto@Abels-MacBook-Pro.local>
2024-06-21 11:25:07 -0600

    feat: Added endpoint for verifyrin images

--- professional-headshot-back/main.py
+++ professional-headshot-back/main.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify
 import os
 import json
 import re
-import threading
 import queue
 from flask_cors import CORS
 from functions.run_training import RunTraining
@@ -12,7 +11,6 @@ from functions.get_images import GetImages
 from functions.upload_images import UploadImages
 from functions.delete_images import DeleteImages
 from functions.upload_model_info import UploadModelInfo
-from functions.filter_images import FilterImages
 
 
 app = Flask(__name__)
@@ -20,6 +18,64 @@ CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})  # Allow 
 UPLOAD_FOLDER = 'uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 plot_request_queue = queue.Queue()
+
+# Train the model
+
+@app.route('/api/start-training', methods=['POST'])
+def start_training():
+    data = request.json
+    api_key = data.get('apiKey')
+    job_name = data.get('jobName')
+    classname = data.get('classname')
+    path = data.get('imagesInBucketPath') + '/'
+    bucket_name = 'backend-professional-headshot-test-avahi'
+    e_mail = re.sub('@*', '', data.get('userEmail'))
+    phone = data.get('phoneNumber')
+    object_prefix = f"{str(data.get('userID'))}-{e_mail}/model-info/"
+ 
+    # Set API KEY
+    os.environ['ASTRIA_API_TOKEN'] = api_key
+
+    response_json = (
+        RunTraining(
+            job_name=job_name,
+            classname=classname,
+            path=path
+        )
+        .process()
+        .get()
+    )
+
+    (
+        UploadModelInfo(
+            response_json=response_json,
+            bucket=bucket_name,
+            object_prefix=object_prefix
+        )
+        .process()
+        .get()
+    )
+
+    return jsonify(response_json), 200
+
+# Return available models
+
+@app.route('/api/get-ids', methods=['POST']) 
+async def get_ids():
+    data = request.json
+    bucket = 'backend-professional-headshot-test-avahi'
+    e_mail = re.sub('@*', '', data.get('userEmail'))
+    path = f"{str(data.get('userID'))}-{e_mail}/model-info/"
+
+    response_dict = (
+        GetModelInfo(
+            bucket=bucket,
+            path=path
+        )
+        .process()
+        .get()
+    )
+    return jsonify({'available_models': response_dict}), 200
 
 # Generate images
 
@@ -30,10 +86,10 @@ def generate_images():
     prompt = data.get('prompt')
     job_id = data.get('jobID')
     classname = data.get('classname')
-    e_mail = re.sub('@*', '', data.get('userEmail'))
-    phone = data.get('phoneNumber')
-    object_prefix = f"{str(data.get('userID'))}-{e_mail}/generated-images/"
-    bucket_name = 'backend-professional-headshot-test-avahi'
+    # e_mail = re.sub('@*', '', data.get('userEmail'))
+    # phone = data.get('phoneNumber')
+    # object_prefix = f"{str(data.get('userID'))}-{e_mail}/generated-images/"
+    # bucket_name = 'backend-professional-headshot-test-avahi'
 
     def generate_thread(
             api_key: str,
@@ -82,8 +138,44 @@ def generate_images():
         job_id=job_id
     )
 
-    plot_request_queue.put(True)
-    selected_images = plot_worker()
+    # plot_request_queue.put(True)
+    # selected_images = plot_worker()
+
+    # (
+    #     UploadImages(
+    #         images=selected_images,
+    #         bucket=bucket_name,
+    #         object_prefix=object_prefix)
+    #     .process()
+    #     .get()
+    # )
+
+    # (
+    #     DeleteImages(images=images)
+    #     .process()
+    #     .get()
+    # )
+
+    # link_to_images = f"https://{bucket_name}.s3.amazonaws.com/{object_prefix}"
+
+    return jsonify(
+        {
+            'message': 'Image Generation is completed',
+            'path': f'Path to images {images}'
+        }
+    ), 200
+
+
+# Verify the images
+
+@app.route('/api/verify-images', methods=['POST'])
+def verify_images():
+    data = request.json
+    images = data.get('allImages')
+    selected_images = data.get('selectedImages')
+    e_mail = re.sub('@*', '', data.get('userEmail'))
+    object_prefix = f"{str(data.get('userID'))}-{e_mail}/generated-images/"
+    bucket_name = 'backend-professional-headshot-test-avahi'
 
     (
         UploadImages(
@@ -109,80 +201,6 @@ def generate_images():
         }
     ), 200
 
-# Train the model
-
-@app.route('/api/start-training', methods=['POST'])
-def start_training():
-    data = request.json
-    api_key = data.get('apiKey')
-    job_name = data.get('jobName')
-    classname = data.get('classname')
-    path = data.get('imagesInBucketPath') + '/'
-    bucket_name = 'backend-professional-headshot-test-avahi'
-    e_mail = re.sub('@*', '', data.get('userEmail'))
-    phone = data.get('phoneNumber')
-    object_prefix = f"{str(data.get('userID'))}-{e_mail}/model-info/"
-    # object_prefix = str(data.get('userID')) + '/model-info/'
- 
-    # Set API KEY
-    os.environ['ASTRIA_API_TOKEN'] = api_key
-
-    response_json = (
-        RunTraining(
-            job_name=job_name,
-            classname=classname,
-            path=path
-        )
-        .process()
-        .get()
-    )
-
-    (
-        UploadModelInfo(
-            response_json=response_json,
-            bucket=bucket_name,
-            object_prefix=object_prefix
-        )
-        .process()
-        .get()
-    )
-
-    return jsonify(response_json), 200
-
-# Return available models
-
-@app.route('/api/get-ids', methods=['POST']) 
-async def get_ids():
-    data = request.json
-    bucket = 'backend-professional-headshot-test-avahi'
-    e_mail = re.sub('@*', '', data.get('userEmail'))
-    path = f"{str(data.get('userID'))}-{e_mail}/model-info/"
-    # path = str(data.get('userID')) + '/model-info/'
-
-    response_dict = (
-        GetModelInfo(
-            bucket=bucket,
-            path=path
-        )
-        .process()
-        .get()
-    )
-    return jsonify({'available_models': response_dict}), 200
-
-def plot_worker():
-    # Wait for a plot request
-    while True:
-        plot_request_queue.get()
-        directory = os.getcwd()
-        selected_images = (
-        FilterImages(folder_path=directory)
-        .process()
-        .get()
-        )
-        return selected_images
-
 if __name__ == '__main__':
-    plot_thread = threading.Thread(target=plot_worker, daemon=True)
-    plot_thread.start()
 
     app.run(debug=True, port=5000)

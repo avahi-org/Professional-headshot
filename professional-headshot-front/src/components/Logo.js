028c6255bf98ca74f6840f25bac71380f9eee15c
gentritrashiti1 <Gentrit.R@avahitech.com>
2024-06-26 02:31:07 +0200

    add logo component

---
+++ professional-headshot-front/src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Logo = () => (
+  <img
+    src="https://awsmp-logos.s3.amazonaws.com/69db6b10-223d-47a0-bb38-e501668f9188/6bac33d0ef3ecb9cc4e75f267d1ce3bf.png"
+    alt="Logo"
+    className="h-12 w-auto mr-4"
+    loading="lazy"
+  />
+);
+
+export default Logo;
